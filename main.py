483021dc9a26c1d0395cf477391c31e9e97980f6
dPenedo <dpenedo@tutanota.com>
2024-01-06 15:39:00 -0300

    audio option added!

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 import os
 import subprocess
 
-download_location = "/home/daniel/Descargas/youtube/"
+DOWNLOAD_LOCATION = "/home/daniel/Descargas/youtube/"
+MP3_FORMAT = ["-x", "--audio-format", "mp3"]
+MP4_FORMAT = ["--remux-video", "mp4"]
+CLIPBOARD_PROGRAM = "xclip"
 
 
-def obtener_portapapeles():
-    datos_de_portapapeles = subprocess.run(["xclip", "-o"], capture_output=True, text=True)
-    texto_de_portapapeles = datos_de_portapapeles.stdout
-    return texto_de_portapapeles
 
-url_youtube = obtener_portapapeles()
+def get_clipbloard_data():
+    clipbard_data = subprocess.run([CLIPBOARD_PROGRAM, "-o"], capture_output=True, text=True)
+    clipbard_text = clipbard_data.stdout
+    return clipbard_text
 
-def descargar_video(url_youtube):
+url_youtube = get_clipbloard_data()
+
+def download_video(url, *formato):
     try:
-        output = f"{download_location}%(title)s.%(ext)s"
+        output = f"{DOWNLOAD_LOCATION}%(title)s.%(ext)s"
 
-        subprocess.run(['yt-dlp', "-o", output, url_youtube], check=True)
-        print("Video descargado.")
+        subprocess.run(['yt-dlp', *formato, "-o", output, url], check=True)
+        print("✓ Video downloaded succesfully.")
     except subprocess.CalledProcessError as e:
-        print("Error al descargar el video:", e)
+        print(f"X Error downloading the video: {e}")
 
-descargar_video(url_youtube)
+download_video(url_youtube, *MP3_FORMAT)
 
 
